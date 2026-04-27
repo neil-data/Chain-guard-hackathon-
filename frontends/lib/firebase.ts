01546dbb463c61bae0766b8ddebe52f5d7164af4
BANERJEE NEIL SANDEEP <neilbanerjee2007@gmail.com>
2026-04-27 19:56:18 +0530

    Update firebase.ts

--- frontends/lib/firebase.ts
+++ frontends/lib/firebase.ts
@@ -3,13 +3,7 @@ import { getAnalytics, isSupported, type Analytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAwg_8SXYLMQSjMi2Xk6TJ03lsCWcU41dI",
-  authDomain: "my-projet-94231.firebaseapp.com",
-  projectId: "my-projet-94231",
-  storageBucket: "my-projet-94231.firebasestorage.app",
-  messagingSenderId: "492954895687",
-  appId: "1:492954895687:web:d172a5923206af22af7862",
-  measurementId: "G-HWMTE0RRK5",
+  
 };
 
 export const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
